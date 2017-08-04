0dca7e4250ccbfdc807b3ea6fbc9479d3743b1e1
Abraham Panduro <panduroab@gmail.com>
2017-08-04 09:55:04 -0500

    tablero

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,15 @@
+Given(/^inicie el juego$/) do
+    visit("/")
+end
 
+Then(/^debo ver el tablero$/) do
+    expect(page).to have_xpath("//table[@id=\"tablero\"]")
+end
+
+Given(/^que el tablero tenga (\d+) filas$/) do |filas|
+    expect(page).to have_xpath('//table/tr', count: filas)
+end
+
+Given(/^que el tablero tenga (\d+) celdas$/) do |celdas|
+  expect(page).to have_xpath('//table/tr/td', count: celdas)
+end
