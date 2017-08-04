0b8868312d930f5596bb71039829e1e702b00263
Abraham Panduro <panduroab@gmail.com>
2017-08-04 12:15:08 -0500

    tablas en model

---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require './lib/game.rb'
+describe Game do
+    it "Mostrar tabla del Jugador" do
+        game = Game.new
+        result = game.get_tbl("tblJugador")
+        expect(result).to eq '<table id="tblJugador" border=1>
+        <tr>
+            <td id="jA1"></td>
+            <td id="jA2"></td>
+            <td id="jA3">x</td>
+            <td id="jA4">x</td>
+            <td id="jA5"></td>
+            <td id="jA6"></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td></tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td></tr>
+        <tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+        <tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+        <tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    </table>'
+    end
+end
