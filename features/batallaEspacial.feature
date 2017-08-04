6b7035d4fc73ddde776879ae8b44aceafec1edf4
Abraham Panduro <panduroab@gmail.com>
2017-08-04 11:00:15 -0500

    Ubicar nave en tablero de jugador

--- features/batallaEspacial.feature
+++ features/batallaEspacial.feature
@@ -7,9 +7,12 @@ Scenario: Que el tablero exista y que sea de 6x6
     Given inicie el juego
     And que el tablero tenga 6 filas
     And que el tablero tenga 36 celdas
-    Then debo ver el tablero
-
+    Then debo ver el tablero de "Jugador"
 
 Scenario: Que mi nave este en el tablero en un posicion fija
+    Given inicie el juego
+    And debo ver el tablero de "Jugador"
+    Then debo ver mi nave en la posición "jA3" 
+    And debo ver mi nave en la posición "jA4"
     
 Scenario: Que la nava en el tablero ocupe dos cuadritos
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -3,11 +3,11 @@ Given(/^inicie el juego$/) do
 end
 
 Then(/^debo ver "([^"]*)"$/) do |texto|
-  expect(page.body).to match /#{texto}/m
+    expect(page.body).to match /#{texto}/m
 end
 
-Then(/^debo ver el tablero$/) do
-    expect(page).to have_xpath("//table[@id=\"tablero\"]")
+Then(/^debo ver el tablero de "([^"]*)"$/) do |tablero|
+  expect(page).to have_xpath("//table[@id=\"tbl#{tablero}\"]")
 end
 
 Given(/^que el tablero tenga (\d+) filas$/) do |filas|
@@ -15,5 +15,10 @@ Given(/^que el tablero tenga (\d+) filas$/) do |filas|
 end
 
 Given(/^que el tablero tenga (\d+) celdas$/) do |celdas|
-  expect(page).to have_xpath('//table/tr/td', count: celdas)
+    expect(page).to have_xpath('//table/tr/td', count: celdas)
+end
+
+Then(/^debo ver mi nave en la posición "([^"]*)"$/) do |posicion|
+    #expect(page).to have_xpath("//td[@id=\"#{posicion}\"]")
+    find("//td[@id=\"#{posicion}\"]", :text => "x")
 end
